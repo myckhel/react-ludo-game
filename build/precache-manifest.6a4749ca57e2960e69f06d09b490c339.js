d968a8a46c5420f3e31a5f08b4246446f2e51510
myckhel <myckhel1@hotmail.com>
2019-02-18 11:36:00 +0100

    buit project

---
+++ build/precache-manifest.6a4749ca57e2960e69f06d09b490c339.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/react-ludo-game/static/media/fontawesome-webfontd41d.674f50d2.eot"
+  },
+  {
+    "revision": "54c1bbd54dd579405fc7",
+    "url": "/react-ludo-game/static/css/main.17b7df4d.chunk.css"
+  },
+  {
+    "revision": "acf3dcb7ff752b5296ca23ba2c7c2606",
+    "url": "/react-ludo-game/static/media/fontawesome-webfont3e6e.acf3dcb7.svg"
+  },
+  {
+    "revision": "25743b9e3ac802a7f9c8",
+    "url": "/react-ludo-game/static/js/1.25743b9e.chunk.js"
+  },
+  {
+    "revision": "03b5d7495409b48539fa",
+    "url": "/react-ludo-game/static/js/runtime~main.03b5d749.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/react-ludo-game/static/media/fontawesome-webfont3e6e.674f50d2.eot"
+  },
+  {
+    "revision": "54c1bbd54dd579405fc7",
+    "url": "/react-ludo-game/static/js/main.54c1bbd5.chunk.js"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/react-ludo-game/static/media/fontawesome-webfont3e6e.af7ae505.woff2"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/react-ludo-game/static/media/fontawesome-webfont3e6e.fee66e71.woff"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/react-ludo-game/static/media/fontawesome-webfont3e6e.b06871f2.ttf"
+  },
+  {
+    "revision": "25743b9e3ac802a7f9c8",
+    "url": "/react-ludo-game/static/css/1.d734fc2d.chunk.css"
+  },
+  {
+    "revision": "3e049052830013d030e385a05cd3216a",
+    "url": "/react-ludo-game/index.html"
+  }
+];
